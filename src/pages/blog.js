0bbfc6a4cf77d2edc95a1cf66287588910bf3628
Khalil EL ISMAILI <khalil_simo@hotmail.fr>
2019-07-01 22:20:27 +0200

    blog styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,38 +1,55 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby';
+import { isMobileOnly } from "react-device-detect";
 
+import Col from '../components/content/Col';
 import Layout from '../components/layout/layout';
 import Page from '../components/content/Page';
 import PageTitle from '../components/content/PageTitle';
+import Row from '../components/content/Row';
 import SEO from '../components/seo';
 import Section from '../components/content/Section';
 
+import './blog.css';
+
 const BlogPage = (props) => {
-    const postList = props.data.allMarkdownRemark;
-    return (
-        <Layout>
-            <SEO
-                title="Blog"
-                pathname='/blog/'
-            />
 
-            <Page>
-                <Section>
-                    <PageTitle title="Blog" />
+  const postList = props.data.allMarkdownRemark;
 
-                    {postList.edges.map(({ node }, index) => (
-                        <Link key={index} to={`/blog/${node.frontmatter.path}`} className="link" >
-                            <div className="post-list">
-                                <h1>{node.frontmatter.title}</h1>
-                                <span>{node.frontmatter.date}</span>
-                                <p>{node.excerpt}</p>
-                            </div>
-                        </Link>
-                    ))}
-                </Section>
-            </Page>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <SEO
+        title="Blog"
+        pathname='/blog/'
+      />
+
+      <Page>
+        <Section fullWidth={isMobileOnly}>
+          <PageTitle title="Blog" />
+
+          <Row>
+            {postList.edges.map(({ node }, index) => (
+              <Col xs={12} md={4} lg={6} style={{ padding: '0 1em 2em' }}>
+                <div className="blog-item">
+                  <Link key={index} to={`/blog/${node.frontmatter.path}`} className="link" >
+                    <div className="image-holder">
+                      <img src={node.frontmatter.image} className="image" alt={node.frontmatter.title} />
+                    </div>
+                    <div className="post-info" style={{ padding: '1em 1.4em 1.4em' }}>
+                      <span className="post-date" style={{ color: '#888', fontSize: '11px', fontWeight: '300' }}>{node.frontmatter.date}</span>
+                      <h4>{node.frontmatter.title}</h4>
+                      {/* <p>{node.excerpt}</p> */}
+                    </div>
+                  </Link>
+                </div>
+              </Col>
+            ))}
+
+          </Row>
+        </Section>
+      </Page>
+    </Layout>
+  )
 }
 
 export default BlogPage;
@@ -45,8 +62,9 @@ export const listQuery = graphql`
           excerpt(pruneLength: 250)
           frontmatter {
             path
-            date(formatString: "MMMM Do YYYY")
+            date(formatString: "Do MMMM YYYY", locale: "fr")
             title
+            image
           }
         }
       }
