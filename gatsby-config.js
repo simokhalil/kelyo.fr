c8506b3c3387d67784913fbf1c8c47cb5237c8f7
Khalil EL ISMAILI <khalil_simo@hotmail.fr>
2019-07-09 01:54:33 +0200

    Fixed website description (SEO)

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `Khalil EL ISMAILI`,
-    description: `Consultant Javascript, ReactJS, Angular, VueJS NodeJS`,
+    description: `Développeur Web et Mobile multi-platefomes, je suis consultant sur les nouvelles technos (Javascript, ReactJS / React Native, Angular, NodeJS) sur Nantes et alentours`,
     author: `Khalil EL ISMAILI`,
     siteUrl: `https://www.kelyo.fr`,
   },
