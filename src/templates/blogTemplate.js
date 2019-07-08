74353a95e58c071dd31b3b7f1beee0ddedc32bfd
Khalil <khalil_simo@hotmail.fr>
2019-07-08 18:18:05 +0000

    Develop

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -8,47 +8,46 @@ import SEO from '../components/seo';
 import Section from '../components/content/Section';
 
 const Template = ({ data: { markdownRemark } }) => {
-    const { frontmatter, excerpt, html } = markdownRemark;
+  const { frontmatter, excerpt, html } = markdownRemark;
 
-    return (
-        <Layout>
-            <SEO
-                title={frontmatter.title}
-                pathname={`/blog/${frontmatter.path}`}
-                description={excerpt}
-            />
+  return (
+    <Layout>
+      <SEO
+        title={frontmatter.title}
+        pathname={`/blog/${frontmatter.path}`}
+        description={excerpt}
+      />
 
-            <Page>
-                <Section>
-                    <PageTitle title={frontmatter.title} description={frontmatter.date} />
+      <Page>
+        <Section>
+          <PageTitle title={frontmatter.title} description={frontmatter.date} />
 
-                    <div className="blog-post-container">
-                        <div className="blog-post">
-                            <div
-                                className="blog-post-content"
-                                dangerouslySetInnerHTML={{ __html: html }}
-                            />
-                        </div>
-                    </div>
-                </Section>
-            </Page>
-
-        </Layout>
-    )
-}
+          <div className="blog-post-container">
+            <div className="blog-post">
+              <div
+                className="blog-post-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
+            </div>
+          </div>
+        </Section>
+      </Page>
+    </Layout>
+  );
+};
 
 export default Template;
 
 export const pageQuery = graphql`
-    query BlogPostQuery($uid: String!) {
-        markdownRemark(frontmatter: { path: { eq: $uid } }) {
-            html
-            excerpt(pruneLength: 250)
-            frontmatter {
-                date(formatString: "Do MMMM YYYY", locale: "fr")
-                path
-                title
-            }
-        }
+  query BlogPostQuery($uid: String!) {
+    markdownRemark(frontmatter: { path: { eq: $uid } }) {
+      html
+      excerpt(pruneLength: 250)
+      frontmatter {
+        date(formatString: "Do MMMM YYYY", locale: "fr")
+        path
+        title
+      }
     }
+  }
 `;
